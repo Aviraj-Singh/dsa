bcdb79f2354cb3a69db3451579c1873b1414e989
Aviraj Singh <69676469+Aviraj-Singh@users.noreply.github.com>
2024-03-13 17:45:44 +0530

    Update hashing.py
    added alphabet hashing logic using array, considering small case letters only

--- hashing.py
+++ hashing.py
@@ -1,4 +1,4 @@
-# Hashing
+# Number Hashing
 arr = [1, 2, 3, 3, 3, 2, 0]
 
 def findCount(n, arr):
@@ -13,3 +13,17 @@ print(findCount(2, arr))
 
 # Overall time complexity of the provided code is O(n + m) and space complexity is O(m)
 # where, m is the maximum value in arr and n is the length of the input list arr.
+
+# Charater Hashing
+str = 'abcbcbcad'
+def findAl(char, str):
+    arr_hash = [0]*26 # assuing characcters will be only small letters, if not use 256
+    for i in range(len(str)):
+        arr_hash[ord(str[i])-ord('a')]+=1 #ord is used to get ascii value of the character
+    return arr_hash[ord(char)-ord('a')]
+
+print(findAl('d', str))
+
+# Overall time complexity of the provided code is O(n) and space complexity is O(1)
+# where, n is the length of the input string.
+# The code uses an array arr_hash of size 26 to store the frequency of each lowercase alphabet character. Hence, O(1) space complexity
